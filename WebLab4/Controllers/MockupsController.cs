ad937b2ae425fb181db0df3d9b54f6de234a34f7
Denis Pokrovskiy <12den00@gmail.com>
2021-06-16 03:31:51 +0700

    Initial commit

---
+++ WebLab4/Controllers/MockupsController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using WebLab4.Models;
+
+namespace WebLab4.Controllers
+{
+    public class MockupsController : Controller
+    {
+        SignUp signup;
+        private readonly ILogger<MockupsController> _logger;
+
+        public MockupsController(ILogger<MockupsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Confirm(string value)
+        {
+            SignUp signup = new SignUp();
+            if (Request.Method == "POST")
+            {
+                if (signup.ResetCode == value)
+                    return View("Result2");
+                else ViewBag.Check = "Wrong code!";
+                return View();
+            }
+            else return View();
+        }
+
+        [HttpGet]
+        public IActionResult PassReset()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PassReset(SignUp signup, string action)
+        {
+            if (ModelState["Email"].ValidationState == ModelValidationState.Valid)
+            {
+                if (action == "Send me a code")
+                {
+                    ViewBag.ResetCode = "Code: " + signup.ResetCode;
+                    return View();
+                }
+                return Redirect("Confirm");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult SignUp(SignUp user)
+        {
+            if (ModelState["FirstName"].ValidationState == ModelValidationState.Valid &
+                ModelState["LastName"].ValidationState == ModelValidationState.Valid &
+                ModelState["Gender"].ValidationState == ModelValidationState.Valid)
+                return RedirectToAction("SignUp2", user);
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignUp2()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp2(SignUp user)
+        {
+            if (ModelState["Email"].ValidationState == ModelValidationState.Valid &
+                ModelState["Password"].ValidationState == ModelValidationState.Valid &
+                ModelState["ComparePassword"].ValidationState == ModelValidationState.Valid)
+                return View("Result", user);
+
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebLab4/Models/SignUp.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebLab4.Models
+{
+    public class SignUp
+    {
+        [Required(ErrorMessage = "Enter a FirstName")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Enter a LastName")]
+        public string LastName { get; set; }
+
+        [Required]
+        public int BirthDay { get; set; }
+
+        [Required]
+        public string BirthMonth { get; set; }
+
+        [Required]
+        public int BirthYear { get; set; }
+
+        [Required(ErrorMessage = "Check a Gender")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Enter Email")]
+        [EmailAddress(ErrorMessage = "Incorrect email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Enter a Password")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Mismatch Password")]
+        public string ComparePassword { get; set; }
+
+        public bool Remember { get; set; }
+
+        public string ResetCode { get; set; }
+
+
+        public SignUp()
+        {
+            ResetCode = "2021";
+        }
+
+
+        public void generatenewCode()
+        {
+            Random rand = new Random();
+            ResetCode = "";
+            for (int i = 0; i < 4; i++)
+                ResetCode += rand.Next(1, 9);
+        }
+    }
+}
